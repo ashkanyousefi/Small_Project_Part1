51870ed7928b81879cd4455ac7d3465def33204e
Ashkan Yousefi <ashkan.yousefi@berkeley.edu>
2019-08-02 16:48:28 +1000

    sanbox updated

---
+++ Ash_Sand_Box.py
@@ -0,0 +1,11 @@
+camelton_lines=[] 
+with open('C:/Users/Ash_PC/Documents/GitHub/Small_Project_Part1/camelton.txt') as my_file:
+    for line in my_file:
+        camelton_lines.append(line.strip())
+    print(camelton_lines)
+
+    
+    # print(my_file.readline(10))
+    # print(my_file.read(7))
+
+    
